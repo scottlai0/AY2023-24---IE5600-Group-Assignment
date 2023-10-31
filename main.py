ea7229893d552ae2f699ef9c561f1c4a94d91f67
scottlai0 <scottlai96@hotmail.com>
2023-11-01 07:33:26 +0800

    Updated with vertex and edges

--- main.py
+++ main.py
@@ -1,45 +1,90 @@
-class Node:
-    name: str
-    next: list
+
+class Vertex:
     
-    def __init__(self, name):
-        self.name = name
-        self.next = []
+    def __init__(self, key):
+        self.key = key
+        self.adjacentList = list([])
 
-    def addPath(self, nextNode: Node, distance: int):
-        self.nextNode.append({nextNode: distance})
+    def addNeighbour(self, neighbour, weight=0):        
+        neighbourExist = False
+
+        for adjacentItem in self.adjacentList:
+            if adjacentItem[0].key == neighbour.key:
+                neighbourExist = True
+                break
+
+        if not neighbourExist:
+            self.adjacentList.append((neighbour, weight))
     
-    def toString(self):
-        if len(self.nextNode) == 0:
-            print(f"{self.name} is a sink.")
-        else:
-            print(f"{self.name}")
+    def toString(self):    
+        strData = str(self.key)
+    
+        for adjacentVertex in self.adjacentList:        
+            strData += ' -> ' + str(adjacentVertex[0].key) + ':' + str(adjacentVertex[1])
+
+        return strData
 
 
-class Queue():
+
+class Graph():
+    
+    def __init__(self): 
+        self.vertices = dict({})
+        self.numVertices = 0
+    
+    def addVertex(self, key):  
+        self.numVertices += 1
+        newVertex = Vertex(key)
+        self.vertices[key] = newVertex  
+        return newVertex
+
+    
+    def getVertex(self, key):  
+        if key in self.vertices.keys():      
+            return self.vertices[key]        
+        return None
+    
+    
+    def addEdge(self, fromKey, toKey, weight=0): 
+
+        if not self.has_key(fromKey):     
+            self.addVertex(fromKey)
+        
+        if not self.has_key(toKey):
+            self.addVertex(toKey)
+        
+        self.vertices[fromKey].addNeighbour(self.vertices[toKey], weight)
+    
+    
+    def getVertices(self): 
+        return self.vertices.values()
+    
+    def has_key(self, key):  
+        if key in self.vertices.keys():     
+            return True
+        return False
+
+
+
+class Queue:
     
     def __init__(self):
-        self._items = list([])
-       
+        self._items = list([])       
     
     def empty(self):
         return self.size() == 0
     
-    
     def size(self): 
-        return len(self._items)                
-    
+        return len(self._items)
 
     def enqueue(self, x):        
         self._items.insert(0, x)
-    
     
     def dequeue(self):        
         if not self.empty():            
             return self._items.pop()    
         return None
-    
-    
+       
     def toString(self):        
         return str(self._items)
 
